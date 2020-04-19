e86b5b2abe6b0243667cfdfa248f02d6aa815b7a
doritosxxx <georgyvav@ya.ru>
2020-04-19 12:32:57 +0400

    Update index.php

--- users/get/index.php
+++ users/get/index.php
@@ -3,4 +3,5 @@ define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
 
 $nickname = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
 $token = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_STRING);
-echo $nickname . ' ' . $token;
+
+var_dump($token);
