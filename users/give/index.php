893c8d2bc99cc2ecc5cf0ea4d02e0989fdcd7bc2
doritosxxx <georgyvav@ya.ru>
2020-04-19 13:40:20 +0400

    fix

--- users/give/index.php
+++ users/give/index.php
@@ -4,7 +4,7 @@ require_once(ROOT . 'config.php');
 
 $nickname = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
 $token = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_STRING);
-$delta = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_NUMBER_INT);
+$delta = filter_input(INPUT_GET, 'money', FILTER_SANITIZE_NUMBER_INT);
 
 if($nickname === null || $token === null || $delta === null || $token !== TOKEN)
 	exit("False");
