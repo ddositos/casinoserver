611a02a51a96129a611d44a24502f88667d8c53b
doritosxxx <georgyvav@ya.ru>
2020-04-19 12:45:14 +0400

    -

--- config.php
+++ config.php
@@ -1,11 +1,21 @@
 <?php
-function connect(){
-	$host = 'remotemysql.com:3306';
-	$db = 'HuV3wK2vrx';
-	$username = 'HuV3wK2vrx';
-	$password = '0rUY2FTmY6';
-	return new PDO("mysql:host=$host;dbname=$db;charset=utf8", $username, $password);
+
+class Database{
+	private $pdo = null;
+	function __construct(){
+		$host = 'remotemysql.com:3306';
+		$db = 'HuV3wK2vrx';
+		$username = 'HuV3wK2vrx';
+		$password = '0rUY2FTmY6';
+		$this->pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $username, $password);
+	}
+	function user_create($nickname){
+		$statement = $this->pdo->prepare("SELECT FROM users id WHERE nickname = ?");
+		$statement->execute($nickname);
+		var_dump($statement->fetch(PDO::FETCH_LAZY));
+	}
 }
+
 
 
 define("TOKEN", "pank228");
--- users/get/index.php
+++ users/get/index.php
@@ -4,4 +4,10 @@ define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
 $nickname = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
 $token = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_STRING);
 
-var_dump($token);
+if($nickname === null || $token === null)
+	exit(0);
+
+require_once(ROOT . 'config.php');
+
+$db = new Database();
+$db->user_create($nickname);
