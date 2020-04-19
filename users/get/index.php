5f867eb95263601ed95006714f2722018b95d228
doritosxxx <georgyvav@ya.ru>
2020-04-19 20:29:39 +0400

    Update index.php

--- users/get/index.php
+++ users/get/index.php
@@ -2,7 +2,7 @@
 header("HTTP/1.1 200 OK");
 define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
 require_once(ROOT . 'config.php');
-
+var_dump($_POST);
 $nickname = filter_input(INPUT_POST, 'nickname', FILTER_SANITIZE_STRING);
 $token = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
 
@@ -12,4 +12,4 @@ if($nickname === null || $token === null || $token !== TOKEN)
 $db = new Database();
 echo $db->user_get($nickname);
 
-var_dump($_POST);
+
