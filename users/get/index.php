c363c5381b0f7cace3cbb5872109395ef27681f3
doritosxxx <georgyvav@ya.ru>
2020-04-19 20:02:09 +0400

    post and http200

--- users/get/index.php
+++ users/get/index.php
@@ -1,9 +1,10 @@
 <?php
+header("HTTP/1.1 200 OK");
 define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
 require_once(ROOT . 'config.php');
 
-$nickname = filter_input(INPUT_GET, 'nickname', FILTER_SANITIZE_STRING);
-$token = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_STRING);
+$nickname = filter_input(INPUT_POST, 'nickname', FILTER_SANITIZE_STRING);
+$token = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
 
 if($nickname === null || $token === null || $token !== TOKEN)
 	exit(0);
--- users/pay/index.php
+++ users/pay/index.php
@@ -1,14 +1,15 @@
 <?php
+header("HTTP/1.1 200 OK");
 define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
 require_once(ROOT . 'config.php');
 
-$nickname = filter_input(INPUT_GET, 'nickname', FILTER_SANITIZE_STRING);
-$token = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_STRING);
-$delta = filter_input(INPUT_GET, 'money', FILTER_SANITIZE_NUMBER_INT);
+$nickname = filter_input(INPUT_POST, 'nickname', FILTER_SANITIZE_STRING);
+$token = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
+$delta = filter_input(INPUT_POST, 'money', FILTER_SANITIZE_NUMBER_INT);
 
 if($nickname === null || $token === null || $delta === null || $token !== TOKEN)
 	exit("False");
 
 $db = new Database();
-echo $db->user_pay($nickname, -$delta);
+echo $db->user_pay($nickname, $delta);
 
