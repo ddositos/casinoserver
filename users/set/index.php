e779ce9cf8914111b58f5d394a4c95805e35ee94
doritosxxx <georgyvav@ya.ru>
2020-04-22 15:52:22 +0400

    Update index.php

--- users/set/index.php
+++ users/set/index.php
@@ -5,11 +5,11 @@ require_once(ROOT . 'config.php');
 
 $token = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
 $nickname = filter_input(INPUT_POST, 'nickname', FILTER_SANITIZE_STRING);
-$amount = filter_input(INPUT_POST, 'amount', FILTER_SANITIZE_NUMBER_INT);
+$balance = filter_input(INPUT_POST, 'balance', FILTER_SANITIZE_NUMBER_INT);
 
-if($nickname === null ||  $amount === null || $token !== TOKEN)
+if($nickname === null ||  $balance === null || $token !== TOKEN)
 	exit("False");
 
 $db = new Database();
-echo $db->user_set($nickname, $amount);
+echo $db->user_set($nickname, $balance);
 
